266d9854d6f758f02fe7275ce1a2a458f9371107
sundazekiks <abutontravis1@gmail.com>
2026-03-24 17:06:10 -0400

    finished implementing the login and register pages and api

---
+++ routes/accountRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = new express.Router();
+const accountController = require('../controllers/accountController')
+const utilities = require("../utilities/")
+
+
+router.get('/login', utilities.handleErrors(accountController.buildLogin));
+
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -12,7 +14,33 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require('./routes/inventoryRoute')
+const accountRoute = require('./routes/accountRoute')
 const utilities = require("./utilities/")
+const bodyParser = require('body-parser')
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(require('connect-flash')())
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
@@ -28,6 +56,7 @@ app.set("layout", "./layouts/layout");
 app.use(static)
 app.get('/', utilities.handleErrors(baseController.buildHome));
 app.use('/inv', inventoryRoute)
+app.use('/account', accountRoute)
 
 app.use(async (req, res, next) => {
   next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
